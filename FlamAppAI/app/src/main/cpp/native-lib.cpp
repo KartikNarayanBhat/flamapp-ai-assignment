e3d08c40eda7561ea28f9181f55dbe06b4e55da5
KartikNarayanBhat <1dt22cs071@dsatm.edu.in>
2025-11-23 23:21:40 +0530

    feat: setup Android app with NDK and OpenCV test

--- FlamAppAI/app/src/main/cpp/native-lib.cpp
+++ FlamAppAI/app/src/main/cpp/native-lib.cpp
@@ -1,11 +1,21 @@
 #include <jni.h>
 #include <android/log.h>
+#include <opencv2/core.hpp>
+#include <opencv2/imgproc.hpp>
 
 #define LOG_TAG "FlamAppAI-Native"
 #define LOGI(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
 
+using namespace cv;
+
 extern "C"
 JNIEXPORT void JNICALL
 Java_com_kartik_flamappai_NativeBridge_initNative(JNIEnv* env, jobject thiz) {
-LOGI("Native init called");
+    LOGI("Native init called");
+
+    Mat testImg(100, 100, CV_8UC1, Scalar(0));
+    line(testImg, Point(10, 10), Point(90, 90), Scalar(255), 2);
+    Mat edges;
+    Canny(testImg, edges, 80, 150);
+    LOGI("OpenCV OK: edges size = %d x %d", edges.cols, edges.rows);
 }
